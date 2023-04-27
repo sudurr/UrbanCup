3268f18f6ae6702a2ba6de67849a52bfae8e269e
Sudur Sugunushev <sudursugunushev@gmail.com>
2023-04-27 20:26:01 +0600

    Initial Commit

---
+++ UrbanCup/UrbanCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanCupApp.swift
+//  UrbanCup
+//
+//  Created by Судур Сугунушев on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct UrbanCupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
